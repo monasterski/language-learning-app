81d7a7d06323e3b1ac5cf44c08b28976df465e5c
Mateusz Monasterski <mateusz.mona@interia.pl>
2020-05-19 01:23:54 +0200

    Added Spring Boot server hello world

---
+++ Server/src/main/java/pl/edu/agh/languagelearningserver/LanguageLearningServerApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.languagelearningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LanguageLearningServerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/pl/edu/agh/languagelearningserver/LanguageLearningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.languagelearningserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
