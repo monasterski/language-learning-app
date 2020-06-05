5dff093c8bff3c26b6d008ca1d3527e7fb847bbd
zychallo15 <zychallo15@gmail.com>
2020-06-05 09:32:49 +0200

    Add simple swagger2 documentation

--- Server/src/main/java/pl/edu/agh/languagelearningserver/LanguageLearningServerApplication.java
+++ Server/src/main/java/pl/edu/agh/languagelearningserver/LanguageLearningServerApplication.java
@@ -1,14 +1,44 @@
 package pl.edu.agh.languagelearningserver;
 
+import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class LanguageLearningServerApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LanguageLearningServerApplication.class, args);
+	}
+
+
+	@Bean
+	public Docket get(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(getSwaggerPaths())
+				.build();
+
+	}
+
+	private Predicate<String> getSwaggerPaths(){
+		return or(
+				regex("/word.*"),
+				regex("/category.*"),
+				regex("/user.*"),
+				regex("/quiz.*"),
+				regex("/test.*")
+		);
 	}
 
 }
